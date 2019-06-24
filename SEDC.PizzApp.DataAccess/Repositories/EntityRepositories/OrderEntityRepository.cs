9e17257154d573e04c7114dd14a264fb07edf3e5
dragan <dragang@iborn.net>
2019-06-24 19:50:11 +0200

    update before context

---
+++ SEDC.PizzApp.DataAccess/Repositories/EntityRepositories/OrderEntityRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzApp.DataAccess.Repositories.EntityRepositories
+{
+    class OrderEntityRepository
+    {
+    }
+}
---
+++ SEDC.PizzApp.DataAccess/Repositories/PizzaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzApp.DataAccess.Repositories
+{
+    public class PizzaDbContext : DbContext
+    {
+        public PizzaDbContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
